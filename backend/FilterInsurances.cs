af1eb90e212a4eab20264621ca7232f01b37a904
gabriels17 <gabriel.sighvatsson@gmail.com>
2021-05-28 21:36:20 +0000

    Filter function implemented

--- backend/FilterInsurances.cs
+++ backend/FilterInsurances.cs
@@ -7,6 +7,9 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Net.Http;
+using System.Collections.Generic;
+using backend;
 
 namespace Vordur.Functions
 {
@@ -20,22 +23,33 @@ namespace Vordur.Functions
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             // STEP 1: Fetch all insurances from blob
+            string url = "https://digdevheimaverkefnitemp.blob.core.windows.net/data/insurances.json?sp=r&st=2021-05-16T15:31:57Z&se=2021-06-30T23:31:57Z&spr=https&sv=2020-02-10&sr=c&sig=LCfyzdARmvYEGV95ZJA7DuH4CwNCLVZFKsfPslWB4gE%3D";
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+            var result = await response.Content.ReadAsStringAsync();
+
             // STEP 2: Store query parameter in a variable
+            string category = req.Query["category"];
+
             // STEP 3: Create a list to store insurances
+            var insurances = JsonConvert.DeserializeObject<List<Insurance>>(result);
+            var filteredInsurances = new List<Insurance>();
+
             // STEP 4: Filter through insurances which category matches the query parameter and add them to the list
+            foreach (var i in insurances)
+            {
+                if (!string.IsNullOrEmpty(category) && i.Category.ToLower().Contains(category.ToLower()))
+                {
+                    filteredInsurances.Add(i);
+                }
+            }
+
             // STEP 5: Return the filtered list
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            if (filteredInsurances.Count == 0)
+            {
+                return new OkObjectResult("No insurances match your criteria.");
+            }
+            return new OkObjectResult(filteredInsurances);
         }
     }
 }
---
+++ backend/Insurance.cs
@@ -0,0 +1,10 @@
+namespace backend
+{
+    public class Insurance
+    {
+        public string Type { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
