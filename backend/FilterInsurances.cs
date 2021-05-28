ba4f15cbf9f75ed22e7765d6e571a2584263ff20
gabriels17 <gabriel.sighvatsson@gmail.com>
2021-05-28 13:14:31 +0000

    Added template for filter function

---
+++ backend/FilterInsurances.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Vordur.Functions
+{
+    public static class FilterInsurances
+    {
+        [FunctionName("FilterInsurances")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            // STEP 1: Fetch all insurances from blob
+            // STEP 2: Store query parameter in a variable
+            // STEP 3: Create a list to store insurances
+            // STEP 4: Filter through insurances which category matches the query parameter and add them to the list
+            // STEP 5: Return the filtered list
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
